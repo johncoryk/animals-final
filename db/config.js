ff09c39f771b4a37319427f28739e478a884ae26
johncoryk <coryk0789@gmail.com>
2020-08-07 12:58:31 -0400

    use DATABASE_URL in production

--- db/config.js
+++ db/config.js
@@ -1,16 +1,24 @@
 require('dotenv').config();
-const DB_NAME = process.env.DB_NAME || "animals";
+const DB_NAME = process.env.DB_NAME || 'animals';
 
 const options = {
-  query: (e) => {
+  query: e => {
     console.log(e.query);
   },
 };
 
 const pgp = require('pg-promise')(options);
 
-module.exports = pgp({
-  database: DB_NAME,
-  port: 5432,
-  host: 'localhost',
-});
+const setDatabase = () => {
+  if (process.env.NODE_ENV === 'developement' || !process.env.NODE_ENV) {
+    return pgp({
+      database: DB_NAME,
+      port: 5432,
+      host: 'localhost',
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    return pgp(process.env.DATABASE_URL);
+  }
+};
+
+module.exports = setDatabase();
